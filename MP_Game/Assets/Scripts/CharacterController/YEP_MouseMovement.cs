dce6f8063ae6677812c48588a15336b4a58a5ef7
saggingcube <shadesofwhite1@gmail.com>
2018-02-08 15:34:52 -0700

    MouseMovement

--- MP_Game/Assets/Scripts/CharacterController/YEP_MouseMovement.cs
+++ MP_Game/Assets/Scripts/CharacterController/YEP_MouseMovement.cs
@@ -2,15 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class YEP_MouseMovement : MonoBehaviour {
+public class YEP_MouseMovement : MonoBehaviour
+{
+    YEP_CharacterInput playerInput;
 
-	// Use this for initialization
-	void Start () {
-		
+    
+    public float lookAngle;
+    public float LookSpeed;
+
+    public float tiltAngle;    
+    public float tiltMax = 75f;
+    public float tiltMin = 45f;
+
+    public Transform pivot;
+
+    // Use this for initialization
+    void Start ()
+    {
+        playerInput = GetComponent<YEP_CharacterInput>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
+
+    private void LateUpdate()
+    {
+        HandleCameraMovement();
+    }
+
+    void HandleCameraMovement()
+    {
+        float x = playerInput.mouseX;
+        float y = playerInput.mouseY;
+
+        lookAngle += x * LookSpeed;
+
+        transform.rotation = Quaternion.Euler(0f, lookAngle, 0);
+
+        tiltAngle -= y * LookSpeed;
+        tiltAngle = Mathf.Clamp(tiltAngle, -tiltMax, tiltMax);
+
+        pivot.localRotation = Quaternion.Euler(tiltAngle, 0, 0);
+    }
 }
