7cb7f2ebb7fe0b3e7503541a692870cdc958295e
saggingcube <shadesofwhite1@gmail.com>
2018-02-08 14:16:21 -0700

    Updated Character input

---
+++ MP_Game/Assets/Scripts/CharacterController/YEP_CharacterInput.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YEP_CharacterInput : MonoBehaviour
+{
+
+    public float mouseX;
+    public float mouseY;
+    public float horizontal;
+    public float vertical;
+    public bool mouse1;
+    public bool mouse2; 
+	
+	
+	void Update ()
+    {
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        mouse1 = Input.GetButton("Fire1");
+        mouse2 = Input.GetButton("Fire2");
+    }
+}
