3eb0d9776eba1b86e8d4554d30e069e919591126
saggingcube <shadesofwhite1@gmail.com>
2018-02-10 11:34:34 -0700

    Characeter Movement

--- MP_Game/Assets/Scripts/CharacterController/YEP_Character_PlayerMovement.cs
+++ MP_Game/Assets/Scripts/CharacterController/YEP_Character_PlayerMovement.cs
@@ -2,15 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class YEP_Character_PlayerMovement : MonoBehaviour {
+public class YEP_Character_PlayerMovement : MonoBehaviour
+{
+    YEP_Character_PlayerInput playerInput;
 
-	// Use this for initialization
-	void Start () {
-		
+    public float movementSpeed;
+    public float runSpeed;
+
+    public bool isGrounded;
+    RaycastHit hitInfo;
+
+    // Use this for initialization
+    void Start ()
+    {
+        playerInput = GetComponent<YEP_Character_PlayerInput>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        GroundCheck();
+        HandleGravity();
+        HandleMovement();
 	}
+
+    void GroundCheck()
+    {
+        
+        if (Physics.Raycast(transform.position, -Vector3.up, out hitInfo, 2))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+
+    void HandleGravity()
+    {
+        if(!isGrounded)
+        {
+            transform.position += Physics.gravity * Time.deltaTime;
+        }        
+    }
+
+    void HandleMovement()
+    {
+        transform.Translate(playerInput.horizontal * Time.deltaTime * movementSpeed, 0, playerInput.vertical * Time.deltaTime * movementSpeed);
+    }
+
 }
